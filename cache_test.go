36c3b66bded4bf17eaaeb6497436a667569b9eb9
kkonat <presto@presto.biz.pl>
2023-03-19 23:43:09 +0100

    fix benchmarks

--- cache_test.go
+++ cache_test.go
@@ -5,6 +5,25 @@ import (
 	"testing"
 )
 
+func BenchmarkCacheAdd(b *testing.B) {
+	b.StopTimer()
+	var (
+		d *benchmarkData
+	)
+	var CacheSize = uint32(b.N/2) + 1
+	//var CacheSize = uint32(b.N)
+
+	cache := newCache[benchmarkData](CacheSize)
+
+	reference := make(map[ID]string)
+	b.StartTimer()
+	for n := 0; n < b.N; n++ {
+		d = NewBenchmarkData(n)
+		cache.add(&Item[benchmarkData]{ID: ID(n), Value: d})
+		reference[ID(n)] = d.Str
+	}
+}
+
 func BenchmarkCacheChkAndGet(b *testing.B) {
 	b.StopTimer()
 	var (
@@ -71,9 +90,14 @@ func BenchmarkCacheRemove(b *testing.B) {
 	b.StartTimer()
 	for n := 0; n < b.N; n++ {
 		left := len(reference)
-		item := ID(rand.Intn(left))
-		reference[item] = reference[left-1]
-		reference = reference[:left-1]
-		cache.removeItem(item)
+		which := rand.Intn(left)
+		item := reference[which]
+		if left >= 1 {
+			reference[which] = reference[left-1]
+			reference = reference[:left-1]
+		}
+
+		cache.removeItem(ID(item))
+		// log.Info(item)
 	}
 }
