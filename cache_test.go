e900bade8e136bd6ff61f55320b29c5bb1c1532c
kkonat <presto@presto.biz.pl>
2023-03-19 22:40:08 +0100

    update benchmark

--- cache_test.go
+++ cache_test.go
@@ -7,7 +7,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func BenchmarkCache(b *testing.B) {
+func BenchmarkCacheChkAndGet(b *testing.B) {
 	b.StopTimer()
 	var (
 		d *benchmarkData
@@ -27,9 +27,31 @@ func BenchmarkCache(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		rndNo := ID(rand.Intn(int(numElements)))
 		cache.checkAndGet(rndNo)
+	}
 
-		// rndNo = ID(rand.Intn(int(numElements)))
-		// cache.touch(rndNo)
+	log.Info("-> ", b.N, " iterations. Cache Hit rate: ", cache.GetHitRate(), " %")
+}
+
+func BenchmarkCacheTouch(b *testing.B) {
+	b.StopTimer()
+	var (
+		d *benchmarkData
+	)
+	var CacheSize = uint32(b.N)
+	var numElements = uint32(b.N)
+
+	cache := newCache[benchmarkData](CacheSize)
+
+	reference := make(map[ID]string)
+	for n := 0; n < b.N; n++ {
+		d = NewBenchmarkData(n)
+		cache.add(&Item[benchmarkData]{ID: ID(n), Value: d})
+		reference[ID(n)] = d.Str
+	}
+	b.StartTimer()
+	for n := 0; n < b.N; n++ {
+		rndNo := ID(rand.Intn(int(numElements)))
+		cache.touch(rndNo)
 	}
 
 	log.Info("-> ", b.N, " iterations. Cache Hit rate: ", cache.GetHitRate(), " %")
