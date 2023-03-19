144e1299dd5a9fc030ace84d1048d53793bbc459
kkonat <presto@presto.biz.pl>
2023-03-19 23:13:01 +0100

    update benchmark cache

--- cache_test.go
+++ cache_test.go
@@ -3,8 +3,6 @@ package simpledb
 import (
 	"math/rand"
 	"testing"
-
-	log "github.com/sirupsen/logrus"
 )
 
 func BenchmarkCacheChkAndGet(b *testing.B) {
@@ -23,13 +21,13 @@ func BenchmarkCacheChkAndGet(b *testing.B) {
 		cache.add(&Item[benchmarkData]{ID: ID(n), Value: d})
 		reference[ID(n)] = d.Str
 	}
+
 	b.StartTimer()
+
 	for n := 0; n < b.N; n++ {
 		rndNo := ID(rand.Intn(int(numElements)))
 		cache.checkAndGet(rndNo)
 	}
-
-	log.Info("-> ", b.N, " iterations. Cache Hit rate: ", cache.GetHitRate(), " %")
 }
 
 func BenchmarkCacheTouch(b *testing.B) {
@@ -53,6 +51,29 @@ func BenchmarkCacheTouch(b *testing.B) {
 		rndNo := ID(rand.Intn(int(numElements)))
 		cache.touch(rndNo)
 	}
+}
 
-	log.Info("-> ", b.N, " iterations. Cache Hit rate: ", cache.GetHitRate(), " %")
+func BenchmarkCacheRemove(b *testing.B) {
+	b.StopTimer()
+	var (
+		d *benchmarkData
+	)
+	var CacheSize = uint32(b.N)
+
+	cache := newCache[benchmarkData](CacheSize)
+
+	reference := []int{}
+	for n := 0; n < b.N; n++ {
+		d = NewBenchmarkData(n)
+		cache.add(&Item[benchmarkData]{ID: ID(n), Value: d})
+		reference = append(reference, n)
+	}
+	b.StartTimer()
+	for n := 0; n < b.N; n++ {
+		left := len(reference)
+		item := ID(rand.Intn(left))
+		reference[item] = reference[left-1]
+		reference = reference[:left-1]
+		cache.removeItem(item)
+	}
 }
